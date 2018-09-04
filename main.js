a57a54c83581b7779eb76524f75ab8373f0cd731
Chelsea Stauber <chelsea@Chelseas-MacBook-Pro.local>
2018-09-04 01:43:35 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(e) {
+
+    // $("input").click(function(){
+    //     var checked =$(this).prop('checked')
+    //     if(checked) {
+    //         console.log(checked);
+    //     }
+        
+    //     $(".content").animate({height:0},200);
+    //     // var checkBoxes = $("input");
+    //     // checkBoxes.prop("checked", !checkBoxes.prop("checked"));
+        
+    // })
+
+});
